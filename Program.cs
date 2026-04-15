31a4dfeb5732ade6bb0aca6b69fac91957417249
BRYANT CAMILO <bhcastro1101@gmail.com>
2026-04-15 18:31:53 -0500

    Ajuste de Program.cs

--- Program.cs
+++ Program.cs
@@ -7,19 +7,19 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// --- CAMBIO CLAVE PARA PERMISOS EN AZURE ---
 var path = Path.Combine(Path.GetTempPath(), "TaskMaster.db");
 builder.Services.AddSqlite<AppDbContext>($"Data Source={path}");
-
+// -------------------------------------------
 
 var app = builder.Build();
 
-// --- ESTO ARREGLA EL ERROR 500 EN AZURE ---
+// --- ESTO ASEGURA QUE LA BASE DE DATOS SE CREE AL ARRANCAR ---
 using (var scope = app.Services.CreateScope())
 {
     var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-    context.Database.EnsureCreated(); // Crea la base de datos si no existe en el servidor
+    context.Database.EnsureCreated();
 }
-// ------------------------------------------
 
 app.UseSwagger();
 app.UseSwaggerUI(c =>
@@ -31,3 +31,4 @@ app.UseSwaggerUI(c =>
 app.UseHttpsRedirection();
 app.MapControllers();
 app.Run();
+
