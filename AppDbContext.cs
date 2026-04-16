737ba8e8ee3c89e63172d70bc2683e01e3828286
BRYANT CAMILO <bhcastro1101@gmail.com>
2026-04-15 19:15:12 -0500

    Cambiando el AppDbContext.cs

--- AppDbContext.cs
+++ AppDbContext.cs
@@ -7,6 +7,6 @@ public class AppDbContext : DbContext
 {
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
-    // Esta línea crea la tabla de base de datos basada en tu clase Tarea
+    // Esta línea crea la tabla de base de datos basada en la clase Tarea
     public DbSet<Tarea> Tareas => Set<Tarea>();
 }
